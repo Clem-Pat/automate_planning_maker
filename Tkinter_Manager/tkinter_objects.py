360cb91f2a3353c5ddb8d2101b22ccf240ec706a
ClemPat <clement.patrizio@outlook.fr>
2023-08-12 17:34:00 +0200

    global path image folder button

--- Tkinter_Manager/tkinter_objects.py
+++ Tkinter_Manager/tkinter_objects.py
@@ -47,7 +47,8 @@ class Tkinter_button(tk.Button):
 
             if self.id >= 1000:
                 self.cursor, self.command = 'hand2', self.choose_file
-                img = Image.open("Ressources/folder_icon.png")
+                path = os.path.dirname(os.path.abspath(__file__))
+                img = Image.open(f"{path[:-16]}/Ressources/folder_icon.png")
                 resized_img = img.resize((20, 20), Image.LANCZOS)
                 photo = ImageTk.PhotoImage(resized_img)
                 self.config(image=photo, cursor='hand2', bg='light blue', font='Arial 11', relief=tk.RAISED, command=self.command)
@@ -65,7 +66,6 @@ class Tkinter_button(tk.Button):
                             font='Arial 11 bold',
                             relief=tk.RAISED, cursor=self.cursor)
                 self.bind('<Button-1>', self.active_cren)
-                self.bind('<Button-2>', self.active_cren)
                 self.bind('<Button-3>', self.active_cren)
                 self.configure_button()
             elif self.id == 35:
@@ -94,11 +94,11 @@ class Tkinter_button(tk.Button):
                             font='Arial 11 bold', relief=tk.RAISED, cursor=self.cursor)
                 self.x, self.y = 1150-offset_x, 630
                 self.bind('<Button-1>', self.send_mail)
-                self.bind('<Button-2>', self.send_mail)
                 self.bind('<Button-3>', self.send_mail)
             elif self.id >= 1000:
                 self.cursor, self.command = 'hand2', self.choose_directory
-                img = Image.open("Ressources/folder_icon2.png")
+                path = os.path.dirname(os.path.abspath(__file__))
+                img = Image.open(f"{path[:-16]}/Ressources/folder_icon2.png")
                 resized_img = img.resize((20, 20), Image.LANCZOS)
                 photo = ImageTk.PhotoImage(resized_img)
                 try: self.config(image=photo)
@@ -123,7 +123,6 @@ class Tkinter_button(tk.Button):
                             font='Arial 10', width=7,
                             relief=tk.RAISED, cursor=self.cursor)
                 self.bind('<Button-1>', self.select_worker)
-                self.bind('<Button-2>', self.select_worker)
                 self.bind('<Button-3>', self.select_worker)
 
         if sys.platform == 'darwin':
@@ -191,7 +190,7 @@ class Tkinter_button(tk.Button):
         event = args[0]
         coeff = 1
         if int(event.num) == 1: coeff = 1
-        elif int(event.num) == 2 or int(event.num) == 3: coeff = -1
+        elif int(event.num) == 3: coeff = -1
         self['text'] = str(max(int(self['text']) + coeff*1, 0))
         if self['text'] == '0':
             self['bg'] = self.disable_bg
@@ -249,7 +248,7 @@ class Tkinter_button(tk.Button):
                     self['relief'] = tk.RAISED
                     self.app.first_worker_selected = None
 
-            elif int(order) == 2 or int(order) == 3:
+            elif int(order) == 3:
                 self.app.first_worker_selected['borderwidth'] = 2
                 self.app.first_worker_selected['relief'] = tk.RAISED
                 self.app.first_worker_selected = None
@@ -287,7 +286,7 @@ class Tkinter_button(tk.Button):
         if int(order) == 1:
             workers = self.app.main_app.planning.workers
             mode = ''
-        elif int(order) == 2 or int(order) == 3 :
+        elif int(order) == 3 :
             mode = f' - mode démo (envoi à {self.app.main_app.data.admin} seulement)'
             for worker in self.app.main_app.planning.workers :
                 if worker.name == self.app.main_app.data.admin:
