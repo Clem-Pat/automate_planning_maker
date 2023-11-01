e878af5e2d95024d39f1a091bbd72f84b5c808e7
ClemPat <clement.patrizio@outlook.fr>
2023-11-02 00:12:39 +0100

    enables_choose_receivers

--- main.py
+++ main.py
@@ -6,23 +6,31 @@ if __name__ == '__main__':
         try: app.refresh()
         except: app.destroyed = True
 
-        if app.buttons[1].state == 'clicked':
-            app.buttons[1].state = 'unclicked'
+        if app.open_window.cren == True:
+            app.open_window.cren = False
             crens = tkinter_window.Tkinter_window('crens', main_app=app)
             while not(crens.destroyed) :
                 try: crens.refresh()
                 except: crens.destroyed = True
 
-        if app.buttons[2].state == 'clicked':
-            app.buttons[2].state = 'unclicked'
+        if app.open_window.resu == True:
+            app.open_window.resu = False
             resu = tkinter_window.Tkinter_window('resu', main_app=app)
             while not (resu.destroyed):
+                if app.open_window.choose_receivers == True:
+                    app.open_window.choose_receivers = False
+                    choose_receivers = tkinter_window.Tkinter_window('choose_receivers', main_app=app, parent_app=resu)
+                    while not (choose_receivers.destroyed):
+                        try: choose_receivers.refresh()
+                        except: choose_receivers.destroyed = True
+
                 try: resu.refresh()
                 except: resu.destroyed = True
 
+       
 
 """
 issues and things to improve : 
-    - faire une légende ds excel
-    - lors de leur création les fichiers excels ne se suppriment pas
+    - Faire en sorte que l'utilisateur puisse importer un planning déjà créé
+    - Faire en sorte que le bot vérifie qu'il parle bien à la bonne personne. Il recommence la manoeuvre si ce n'est pas le cas
 """
